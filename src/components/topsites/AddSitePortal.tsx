76283270c6cabf3f5183911b3fdf06811d2fa352
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-07-28 23:45:53 +0530

    feat: Added add site skeleton

--- src/components/topsites/AddSitePortal.tsx
+++ src/components/topsites/AddSitePortal.tsx
@@ -1,11 +1,27 @@
 import { createPortal } from "react-dom";
 import "./add_site.scss";
+import { MdClose } from "react-icons/md";
 
 const AddSite = ({ closeModal }: { closeModal: () => void }) => {
 	return createPortal(
 		<>
 			<div className="modal-background" onClick={closeModal}></div>
-			<div className="add-site"></div>
+			<div className="add-site">
+				<MdClose className="close-btn" onClick={closeModal}></MdClose>
+				<h2>Add Site</h2>
+				<div className="input-wrapper">
+					<label>Title</label>
+					<input type="text" placeholder="Website Title" />
+				</div>
+				<div className="input-wrapper">
+					<label>URL</label>
+					<input type="text" placeholder="Website Address" />
+				</div>
+				<div className="btns">
+					<button className="cancel">Cancel</button>
+					<button className="save">Save</button>
+				</div>
+			</div>
 		</>,
 		document.querySelector(".modal-portal")!,
 	);
